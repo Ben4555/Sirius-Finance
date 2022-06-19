c5570a84a4c1b607042b64a132dee745b2062ce7
Ben4555 <107808467+Ben4555@users.noreply.github.com>
2022-06-19 23:36:30 +0600

    Add files via upload

---
+++ lib/module/inc-video-carousel.php
@@ -0,0 +1,87 @@
+<div class="container fill">
+	<div id="myCarousel" class="carousel slide">
+		<div class="carousel-inner">
+			<!-- Carousel Slide -->
+			<div class="item active">
+				<div class="fill">
+					<div class="video-con-container">
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<div class="video-btn-play">
+								</div>
+								<div class="video-ico-service">
+								</div>
+								<div class='video-con-animate page-video-1'>
+								</div>
+								<div class='video-img-thumbnail' style="background: url('/img/videos/Introducing.png') no-repeat center; background-size: cover;">
+								</div>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-4">
+								<div class='video-emp-block'>
+								</div>
+								<h2>Introducing Sirius Finance</h2>
+								<h3>Stablecoin, held by institutions and high-net-work risk-averse investors, is always the most crucial segment to lock large and deep liquidity, as it has already been proved by success of Curve Finance..</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Carousel Slide -->
+			<div class="item">
+				<div class="fill">
+					<div class="video-con-container">
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<div class="video-btn-play">
+								</div>
+								<div class="video-ico-service">
+								</div>
+								<div class='video-con-animate page-video-2'>
+								</div>
+								<div class='video-img-thumbnail' style="background: url('/img/videos/Stablecoin.png') no-repeat center; background-size: cover;">
+								</div>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-5">
+								<div class='video-emp-block'>
+								</div>
+								<h2>Non-stablecoin Pools will launch soon on Sirius Finance</h2>
+								<h3>With 200k veSRS voting to agree with launching non-stablecoin pools on Sirius Finance, users will soon be able to access non-stablecoin pools and stake LP tokens from these pools on farms to start earning.</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Carousel Slide -->
+			<div class="item">
+				<div class="fill">
+					<div class="video-con-container">
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<div class="video-btn-play">
+								</div>
+								<div class="video-ico-service">
+								</div>
+								<div class='video-con-animate page-video-3'>
+								</div>
+								<div class='video-img-thumbnail' style="background: url('/img/videos/Vote.png') no-repeat center; background-size: cover;">
+								</div>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-6">
+								<div class='video-emp-block'>
+								</div>
+								<h2>Vote for your best interest with "Sirius Finance"</h2>
+								<h3>The Voting function is now live on Sirius Finance! You can now vote on the farm weight to redistribute the LP rewards among different farms.</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
